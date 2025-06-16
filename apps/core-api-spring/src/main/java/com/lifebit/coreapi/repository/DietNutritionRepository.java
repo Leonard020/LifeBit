05a80c1e50b89a53300113ba1cd9c932ed7c8aa4
sbbaek0306 <sbbaek0306@gmail.com>
2025-06-16 19:37:10 +0900

    식단 관리 기능과 관련된 DietController, MealController, DTO, Entity, Repository 및 Service 클래스 삭제. 더 이상 사용되지 않는 코드 정리.

--- apps/core-api-spring/src/main/java/com/lifebit/coreapi/repository/DietNutritionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.lifebit.coreapi.repository;
-
-import com.lifebit.coreapi.entity.DietNutrition;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface DietNutritionRepository extends JpaRepository<DietNutrition, Long> {
-    Optional<DietNutrition> findByUserId(Long userId);
-} 
